369dd45e84106ec0b71f41b76bd01f26f0301e1c
DeGioSax <https://gitlab.com/DeGioSax?nav_source=navbar>
2019-06-05 11:44:56 +0200

    0.1.2

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 document.addEventListener('deviceready', function(){
   console.log('Device ready, all ready...');
   navigator.geolocation.getCurrentPosition(function(coords){
-    $('<div>').html(coords.latitude + '/' + coords.longitude).appendTo('body');
+    $('<div>').html(coords.coords.latitude + '/' + coords.coords.longitude).appendTo('body');
 
   }, function(){
     $('<div>').html('Error no position').appendTo('body')
